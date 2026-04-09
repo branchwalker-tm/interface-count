b0ae2c2e5e8eba5bc89fa7d252a3049335861ede
Travis Malone <travis@travmalone.com>
2026-04-09 15:36:38 -0500

    Update interface-count.sh

--- interface-count.sh
+++ interface-count.sh
@@ -3,6 +3,7 @@
 PAN_IP="<YOUR_PANORAMA_IP>"
 API_KEY="<YOUR_API_KEY>"
 
+# Get the list of managed devices from Panorama
 curl -k -s -H "X-PAN-KEY: $API_KEY" "https://$PAN_IP/api/?type=op&cmd=%3Cshow%3E%3Cdevices%3E%3Call%3E%3C/all%3E%3C/devices%3E%3C/show%3E" \
 | grep -oP '(?<=serial>)[^<]+|(?<=model>)[^<]+' \
 | paste - - \
@@ -13,28 +14,24 @@ curl -k -s -H "X-PAN-KEY: $API_KEY" "https://$PAN_IP/api/?type=op&cmd=%3Cshow%3E
     result=$(curl -k -s --max-time 5 -H "X-PAN-KEY: $API_KEY" \
         "https://$PAN_IP/api/?type=op&cmd=%3Cshow%3E%3Cinterface%3Eall%3C/interface%3E%3C/show%3E&target=$serial")
     
-    # Count physical interfaces that are in 'up' state
+    # Extract the total count of 'up' physical interfaces
     count=$(echo "$result" | grep -oE 'ethernet[^"]+|<state>up</state>' | grep -B 1 "<state>up</state>" | grep -c "ethernet")
-    
     echo "$count"
     
-    # Parse the XML to output the speed of each active interface
-    echo "$result" | awk '
-        /<entry.*name="ethernet/ { 
-            intf=$0; sub(/.*name="/, "", intf); sub(/".*/, "", intf)
-            state=""; speed=""
+    # Robustly parse XML for name and the speed/duplex/state field
+    # In PAN-OS XML, the 'st' tag often contains the string "speed/duplex/state"
+    echo "$result" | awk -F'[">]' '
+        /<entry name="ethernet/ { 
+            # Get interface name from name="interface" attribute
+            split($0, a, "name=\""); split(a[2], b, "\""); name=b[1] 
         }
-        /<state>/ {
-            state=$0; sub(/.*<state>/, "", state); sub(/<\/state>.*/, "", state)
-        }
-        /<speed>/ {
-            speed=$0; sub(/.*<speed>/, "", speed); sub(/<\/speed>.*/, "", speed)
-        }
-        /<\/entry>/ { 
-            if (state == "up" && intf != "") { 
-                print "  -> " intf " | Speed: " speed
-            } 
-            intf=""
+        /<st>/ {
+            # st tag contains combined info like 1000/full/up
+            val=$0; sub(/.*<st>/, "", val); sub(/<\/st>.*/, "", val)
+            if (val ~ /\/up/) {
+                split(val, parts, "/")
+                print "  -> " name " | Speed: " parts[1]
+            }
         }
     '
 done
